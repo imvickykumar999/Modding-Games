f02b89cd69d69b384950666b23fbe2058eb48407
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-24 19:43:05 +0530

    logs on play

---
+++ resources/bullets.cs
@@ -0,0 +1,8 @@
+
+// C:\Users\Vicky\Desktop\Repository\Modding-Games\Ravenfield\Ravenfield_Data\Managed\Assembly-CSharp.dll
+// https://youtu.be/49SNTTS3yRs?t=235
+
+if (this.configuration.projectilesPerShot > 5)
+{
+    this.configuration.projectilesPerShot = 100;
+}
